459eb31c7c05ddb8ab8fd5dff4820b76f6522067
物理雾里PhysicsInFog <84223012+PhysicsInFog@users.noreply.github.com>
2023-03-31 23:15:08 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ fontMiddle = ImageFont.truetype("fonts/arialbd.ttf", floor(side*0.2))
 fontLarge = ImageFont.truetype("fonts/arialbd.ttf", floor(side*0.35))
 
 
-weight = load(open("data/weight.json"))
 attribute = load(open("data/attribute.json"))
 color = load(open("data/color.json"))
 name = load(open("data/name.json"))
 position = load(open("data/position.json"))
 symbol = load(open("data/symbol.json"))
+weight = load(open("data/weight.json"))
 
 print("Creating...")
 image = Image.new(
@@ -32,25 +32,24 @@ image = Image.new(
 draw = ImageDraw.Draw(image)
 
 print("Writing...")
-# Other elements
 for i in range(118):
     x, y = position[i]
 
     draw.rectangle(
         xy=(
-            width/2-side*10+side*(x+1.075),
-            height/2-side*6+side*(y+1.075),
-            width/2-side*10+side*(x+1.925),
-            height/2-side*6+side*(y+1.925)
+            width/2-side*10+side*(x+43/40),
+            height/2-side*6+side*(y+43/40),
+            width/2-side*10+side*(x+77/40),
+            height/2-side*6+side*(y+77/40)
         ),
         outline=color[attribute[i]],
-        width=floor(side*0.02)
+        width=floor(side*1/50)
     )
 
     draw.text(
         xy=(
-            width/2-side*10+side*(x+1.15),
-            height/2-side*6+side*(y+1.15)
+            width/2-side*10+side*(x+23/20),
+            height/2-side*6+side*(y+23/20)
         ),
         text=str(i+1),
         fill=color[attribute[i]],
@@ -59,8 +58,8 @@ for i in range(118):
 
     draw.text(
         xy=(
-            width/2-side*10+side*(x+1.5)-fontLarge.getsize(symbol[i])[0]/2,
-            height/2-side*6+side*(y+1.425)-fontLarge.getsize(symbol[i])[1]/2,
+            width/2-side*10+side*(x+3/2)-fontLarge.getsize(symbol[i])[0]/2,
+            height/2-side*6+side*(y+57/40)-fontLarge.getsize(symbol[i])[1]/2,
         ),
         text=symbol[i],
         fill=color[attribute[i]],
@@ -69,8 +68,8 @@ for i in range(118):
 
     draw.text(
         xy=(
-            width/2-side*10+side*(x+1.5)-fontSmall.getsize(name[i])[0]/2,
-            height/2-side*6+side*(y+1.675)-fontSmall.getsize(name[i])[1]/2,
+            width/2-side*10+side*(x+3/2)-fontSmall.getsize(name[i])[0]/2,
+            height/2-side*6+side*(y+67/40)-fontSmall.getsize(name[i])[1]/2,
         ),
         text=name[i],
         fill=color[attribute[i]],
@@ -79,8 +78,8 @@ for i in range(118):
 
     draw.text(
         xy=(
-            width/2-side*10+side*(x+1.5)-fontSmall.getsize(weight[i])[0]/2,
-            height/2-side*6+side*(y+1.8)-fontSmall.getsize(weight[i])[1]/2,
+            width/2-side*10+side*(x+3/2)-fontSmall.getsize(weight[i])[0]/2,
+            height/2-side*6+side*(y+9/5)-fontSmall.getsize(weight[i])[1]/2,
         ),
         text=weight[i],
         fill=color[attribute[i]],
@@ -88,22 +87,21 @@ for i in range(118):
     )
 
 
-# Lanthanide elements
 draw.rectangle(
     xy=(
-        width/2-side*10+side*(2+1.075),
-        height/2-side*6+side*(5+1.075),
-        width/2-side*10+side*(2+1.925),
-        height/2-side*6+side*(5+1.925)
+        width/2-side*10+side*(2+43/40),
+        height/2-side*6+side*(5+43/40),
+        width/2-side*10+side*(2+77/40),
+        height/2-side*6+side*(5+77/40)
     ),
     outline=color["lanthanoid"],
-    width=floor(side*0.02)
+    width=floor(side*1/50)
 )
 
 draw.text(
     xy=(
-        width/2-side*10+side*(2+1.15),
-        height/2-side*6+side*(5+1.15)
+        width/2-side*10+side*(2+23/20),
+        height/2-side*6+side*(5+23/20)
     ),
     text="57-71",
     fill=color["lanthanoid"],
@@ -112,8 +110,8 @@ draw.text(
 
 draw.text(
     xy=(
-        width/2-side*10+side*(2+1.5)-fontMiddle.getsize("La-Lu")[0]/2,
-        height/2-side*6+side*(5+1.45)-fontMiddle.getsize("La-Lu")[1]/2,
+        width/2-side*10+side*(2+3/2)-fontMiddle.getsize("La-Lu")[0]/2,
+        height/2-side*6+side*(5+29/20)-fontMiddle.getsize("La-Lu")[1]/2,
     ),
     text="La-Lu",
     fill=color["lanthanoid"],
@@ -122,8 +120,8 @@ draw.text(
 
 draw.text(
     xy=(
-        width/2-side*10+side*(2+1.5)-fontSmall.getsize("Lanthanoids")[0]/2,
-        height/2-side*6+side*(5+1.7375)-fontSmall.getsize("Lanthanoids")[1]/2,
+        width/2-side*10+side*(2+3/2)-fontSmall.getsize("Lanthanoids")[0]/2,
+        height/2-side*6+side*(5+139/80)-fontSmall.getsize("Lanthanoids")[1]/2,
     ),
     text="Lanthanoids",
     fill=color["lanthanoid"],
@@ -131,22 +129,21 @@ draw.text(
 )
 
 
-# Actinide elements
 draw.rectangle(
     xy=(
-        width/2-side*10+side*(2+1.075),
-        height/2-side*6+side*(6+1.075),
-        width/2-side*10+side*(2+1.925),
-        height/2-side*6+side*(6+1.925)
+        width/2-side*10+side*(2+43/40),
+        height/2-side*6+side*(6+43/40),
+        width/2-side*10+side*(2+77/40),
+        height/2-side*6+side*(6+77/40)
     ),
     outline=color["actinoid"],
-    width=floor(side*0.02)
+    width=floor(side*1/50)
 )
 
 draw.text(
     xy=(
-        width/2-side*10+side*(2+1.15),
-        height/2-side*6+side*(6+1.15)
+        width/2-side*10+side*(2+23/20),
+        height/2-side*6+side*(6+23/20)
     ),
     text="89-103",
     fill=color["actinoid"],
@@ -155,8 +152,8 @@ draw.text(
 
 draw.text(
     xy=(
-        width/2-side*10+side*(2+1.5)-fontMiddle.getsize("Ac-Lr")[0]/2,
-        height/2-side*6+side*(6+1.45)-fontMiddle.getsize("Ac-Lr")[1]/2,
+        width/2-side*10+side*(2+3/2)-fontMiddle.getsize("Ac-Lr")[0]/2,
+        height/2-side*6+side*(6+29/20)-fontMiddle.getsize("Ac-Lr")[1]/2,
     ),
     text="Ac-Lr",
     fill=color["actinoid"],
@@ -165,8 +162,8 @@ draw.text(
 
 draw.text(
     xy=(
-        width/2-side*10+side*(2+1.5)-fontSmall.getsize("Actinoids")[0]/2,
-        height/2-side*6+side*(6+1.7375)-fontSmall.getsize("Actinoids")[1]/2,
+        width/2-side*10+side*(2+3/2)-fontSmall.getsize("Actinoids")[0]/2,
+        height/2-side*6+side*(6+139/80)-fontSmall.getsize("Actinoids")[1]/2,
     ),
     text="Actinoids",
     fill=color["actinoid"],
